4ff2c3f72796a89fcce0be5f634e503b6df7c24e
= <=>
2020-07-22 14:57:38 +0200

    add emby proto

---
+++ scripts/emby/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# https://hub.docker.com/r/emby/embyserver/
+
+sudo docker run -d \
+    --name emby \
+    -v "$(pwd)/config:/config" \
+    -v /media/usb:/media/usb \
+    --device /dev/dri:/dev/dri \
+    -p 8096:8096 \
+    -p 8920:8920 \
+    --env UID=1000 \
+    --env GID=1000 \
+    --env GIDLIST=100 \
+    emby/embyserver_arm32v7:latest
